66ebc1e4151ca8d26d5ac38e479be2638cca2072
Matthieu <matthieu.dufour3@etu.univ-lorraine.fr>
2018-11-06 11:39:21 +0100

    Ajout d'un template pour les pages + page home

---
+++ resources/views/default.blade.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Matthieu Dufour">
+    <meta name="author" content="Théo Borg">
+    <meta name="author" content="Paul Félix">
+    <meta name="author" content="Yannis Mally">
+
+    <title>@yield('title')</title>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('/css/general.css') }}">
+        @yield('link')
+</head>
+
+  <body style="padding-top:50px">
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">MyGiftBox</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Catalogue</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Créer un coffret</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ">
+
+        <!-- Si déconnecté -->
+          <li class="nav-item">
+            <a class="nav-link" href="#">Se connecter</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">S'inscrire</a>
+          </li>
+        <!-- Fin -->
+
+        <!-- Si connecté -->
+          <li class="nav-item">
+            <a class="nav-link" href="#">Déconnexion</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Profil</a>
+          </li>
+        <!-- Fin -->
+
+        </ul>
+      </div>
+    </nav>
+
+    <main role="main">
+
+      @yield('content')
+
+    </main><!-- /.container -->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('default')
+
+
+@section('title')
+    Accueil
+@endsection
+
+
+@section('link')
+    <link rel="stylesheet" href="{{ asset('/css/home.css') }}">
+@endsection
+
+
+@section('content')
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="display-3">My Gift Box</h1>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="grid-container">
+            <button class="grid-item grid-item1" href="#">Toutes les catégories</button>
+            <button class="grid-item grid-item" href="#">Restauration</button>
+            <button class="grid-item grid-item" href="#">Hébergement</button>
+            <button class="grid-item grid-item" href="#">Attention</button>
+            <button class="grid-item grid-item" href="#">Activité</button>
+        </div>
+    </div>
+@endsection
+
