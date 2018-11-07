5a96e64dddd015f66fb3b48b77330f7a6532f320
Loic <loic.spacher@gmail.com>
2018-11-07 17:49:07 +0100

    mise en forme coffret + bouton retour

--- resources/views/coffret.blade.php
+++ resources/views/coffret.blade.php
@@ -7,28 +7,31 @@
 
 
 @section('link')
-    <link rel="stylesheet" href="{{ asset('/css/coffret.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/catalogue.css') }}">
 @endsection
 
 
 @section('content')
-<div class="container">
-        <h1 class="display-3 text-center">Coffret</h1>
-    </div>
+
 
     @foreach($box as $b)
     @isset(Auth::user()->id)
         @if($b->users_id == Auth::user()->id )
+        <div class="container">
+        <h1 class="display-3 text-center">{{ $b->nom }}</h1>
+        </div>
         <div class="container">                             
             <div class="grid-vertical">
                 <div class="grid-coffret">
-                    <h2>{{ $b->nom }}</h2>
                     <p>{{ $b->etat }}</p>
-                    <p>{{ $b->montantTotal }}</p>
+                    <p>{{ $b->montantTotal }}€</p>
                     @isset($prestas)
                         @for($i=0; $i < count($prestas); $i++)
+                        <div class="grid-article">
                         <a href="/gift/{{ $prestas[$i][0]->id }}"><img src="{{ asset('img/'.$prestas[$i][0]->img) }}"></a>
-                        <p>{{ $prestas[$i][0]->nom }}</p>
+                        <a class="nom" href="/gift/{{ $prestas[$i][0]->id }}"><p>{{ $prestas[$i][0]->nom }}</p></a>
+                        <p class="prix">{{ $prestas[$i][0]->prix }}€</p>
+                        </div>
                         @endfor
                     @endisset
                 </div>
@@ -49,7 +52,7 @@
     @endempty
 
     @endforeach
-
+    <a href="{{ url('profile') }}"><button type="button" class="btn btn-secondary">Retour</button></a>
         </div>
     </div>
 @endsection
