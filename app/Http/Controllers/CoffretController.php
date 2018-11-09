96449fed9963bc1b17d0e369bc245fc4039309f8
Loic <loic.spacher@gmail.com>
2018-11-09 14:25:39 +0100

    test

--- app/Http/Controllers/CoffretController.php
+++ app/Http/Controllers/CoffretController.php
@@ -77,7 +77,126 @@ class CoffretController extends Controller
         return CoffretController::afficher($id_coffret);
     }
 
+
+
     public function coffretValidate($id_coffret){
+        $box = DB::table('coffret')
+        ->where('id', $id_coffret)->get();
+
+        $panier = DB::table('panier')
+        ->where('id_coffret', $id_coffret)->get();
+
+        $count = DB::table('panier')
+        ->where('id_coffret', $id_coffret)->count();
+
+        $cat1=$cat2=$cat3=$cat4=0;
+
+        //return view('coffretValidate', compact('box'));
+
+        $err = 'Coffret invalide: doit contenir au moins deux prestations de deux catégorie différente.';
+
+        if($count>=2){
+            for($i=0;$i<$count;$i++){
+                $presta = DB::table('prestation')
+                ->where('id',$panier[$i]->id_prestation)->get();
+                switch ($presta[0]->cat_id) {
+                    case 1:
+                        $cat1+=1;
+                        break;
+                    case 2:
+                        $cat2+=1;
+                        break;
+                    case 3:
+                        $cat3+=1;
+                        break;
+                    case 4:
+                        $cat4+=1;
+                        break;
+                }
+            }
+            //test to know if there's is 2 different categories for the prestations
+            if($cat1>=1 && $cat2 >=1 || $cat2>=1 && $cat3 >=1 || $cat3>=1 && $cat4 >=1 || $cat1>=1 && $cat3 >=1 || $cat1>=1 && $cat4 >=1 || $cat2>=1 && $cat4 >=1){ 
+                return view('coffret', compact('box'));
+             }
+            else{
+                return view('coffret', compact('box'), compact('err'));
+            }
+        }
+        else{
+            return view('coffret', compact('box'),compact('err'));
+        }
+
+    }
+
+    public function validateBox( $id_coffret){
+
+        DB::table('coffret')
+        ->where('id', $id_coffret)
+        ->update(['etat'=> "En attente de paiement"]);
+
+        $box = DB::table('coffret')
+        ->where('id', $id_coffret)->get();
+
+        $panier = DB::table('panier')
+        ->where('id_coffret', $id_coffret)->get();
+
+        $count = DB::table('panier')
+        ->where('id_coffret', $id_coffret)->count();
+
+        $cat1=$cat2=$cat3=$cat4=0;
+
+        //return view('coffretValidate', compact('box'));
+
+        $err = 'Coffret invalide: doit contenir au moins deux prestations de deux catégorie différente.';
+
+        if($count>=2){
+            for($i=0;$i<$count;$i++){
+                $prestas = DB::table('prestation')
+                ->where('id',$panier[$i]->id_prestation)->get();
+                switch ($prestas[0]->cat_id) {
+                    case 1:
+                        $cat1+=1;
+                        break;
+                    case 2:
+                        $cat2+=1;
+                        break;
+                    case 3:
+                        $cat3+=1;
+                        break;
+                    case 4:
+                        $cat4+=1;
+                        break;
+                }
+            }
+            //test to know if there's is 2 different categories for the prestations
+            if($cat1>=1 && $cat2 >=1 || $cat2>=1 && $cat3 >=1 || $cat3>=1 && $cat4 >=1 || $cat1>=1 && $cat3 >=1 || $cat1>=1 && $cat4 >=1 || $cat2>=1 && $cat4 >=1){ 
+                return view('coffret', compact('box'), compact('prestas'));
+             }
+            else{
+                return view('coffret', compact('box'), compact('prestas'), compact('err'));
+            }
+        }
+        else{
+            return view('coffret', compact('box'), compact('prestas'), compact('err'));
+        }
+
+
+
+
+        /* Génération de l'url */
+        $nomcoffret = DB::table('coffret')
+        ->where('id', $id_coffret)
+        ->select('nom')->get();
+        $url = '/ouvrirCoffret/'.sha1(''.Auth::user()->nom.$id_coffret.$nomcoffret[0]->nom);
+        DB::table('coffret')
+        ->where('id', $id_coffret)
+        ->update(['url' => $url]);
+
+        //return CoffretController::afficher($id_coffret);
+        return CoffretController::coffretValidate($id_coffret);
+    }
+
+        /*public function coffretValidate($id_coffret){
         $box = DB::table('coffret')
         ->where('id', $id_coffret)->get();
 
@@ -85,7 +204,7 @@ class CoffretController extends Controller
         ->where('id_coffret', $id_coffret);
         
 
-      /* if(count($panier->id_prestation)>=2){
+       if(count($panier->id_prestation)>=2){
            $diffCat = false;
             $presta = DB::table('prestation')
             ->where('id', $id_prestation)->get();
@@ -95,15 +214,15 @@ class CoffretController extends Controller
                     break;
                 }
             }
-            if($diffCat){ */
+            if($diffCat){ 
                 return view('coffretValidate', compact('box'));
-            /* }
+             }
             else{
                 return view('coffret', compact('box'));
             }
-        }*/
+        }
 
-    }
+}*/
 
     /*public function coffretValidatePost(Request $request){
         
@@ -134,20 +253,4 @@ class CoffretController extends Controller
         return view('coffret', compact('box'));
     }*/
 
-    public function validateBox( $id_coffret){
-        DB::table('coffret')
-        ->where('id', $id_coffret)
-        ->update(['etat'=> "En attente de paiement"]);
-
-        /* Génération de l'url */
-        $nomcoffret = DB::table('coffret')
-        ->where('id', $id_coffret)
-        ->select('nom')->get();
-        $url = '/ouvrirCoffret/'.sha1(''.Auth::user()->nom.$id_coffret.$nomcoffret[0]->nom);
-        DB::table('coffret')
-        ->where('id', $id_coffret)
-        ->update(['url' => $url]);
-
-        return CoffretController::afficher($id_coffret);
-    }
 }
